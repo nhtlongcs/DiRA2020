3f86fb4b4952c66e103c61c7980f715a2c61d85a
kento <18120134@student.hcmus.edu.vn>
2019-11-16 10:40:39 +0700

    base line

--- src/lane_detect/cmake-build-debug/CMakeFiles/api_lane_detection.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for api_lane_detection.
-# This may be replaced when dependencies are built.
--- src/lane_detect/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kvu/Desktop/catkin_ws/src/lane_detect
-# Build directory: /home/kvu/Desktop/catkin_ws/src/lane_detect/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
