6e8f29123e166c9a933bfcb61435565edb5ca48f
kento <18120134@student.hcmus.edu.vn>
2020-01-02 18:11:12 +0700

    update download script
    Former-commit-id: ea4d302a670461323bbb64c9f67a33939671d8eb

--- mobilenet/model/download.sh
+++ mobilenet/model/download.sh
@@ -1,7 +1,32 @@
 #!/bin/bash
 
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=17zgFE_BRWVVqoPRbAjZscu2ZYdNT0mBf' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=17zgFE_BRWVVqoPRbAjZscu2ZYdNT0mBf" -O weight_dira.h5 && rm -rf /tmp/cookies.txt
+declare -a fileid=(
+    "17zgFE_BRWVVqoPRbAjZscu2ZYdNT0mBf"
+    "1lEPCqdJG6YLr0kJVViCfON1Bct6AxaIu"
+    "1FiOJmCB4i1cE012ga4t7wHIezApht4HC"
+    "1qZcSi9mh1qR2RcsIBzfpxNSOKfwOxYsR"
+    )
 
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1lEPCqdJG6YLr0kJVViCfON1Bct6AxaIu' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1lEPCqdJG6YLr0kJVViCfON1Bct6AxaIu" -O model_dira_v2.h5 && rm -rf /tmp/cookies.txt
+declare -a filename=(
+    "weight_dira.h5"
+    "model_dira_v2.h5"
+    "model_dira_v3.h5"
+    "model_dira_v4.h5"
+    )
 
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1FiOJmCB4i1cE012ga4t7wHIezApht4HC' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1FiOJmCB4i1cE012ga4t7wHIezApht4HC" -O model_dira_v3.h5 && rm -rf /tmp/cookies.txt
+# get length of an array
+fileid_len=${#fileid[@]}
+filename_len=${#filename[@]}
+
+if [ ! $fileid_len -eq $filename_len ]; then
+    echo "Length of fileid and filename must match"
+    exit -1
+fi
+
+# use for loop to read all values and indexes
+for (( i=0; i<${fileid_len}; i++ ));
+do
+    echo "Downloading " ${filename[$i]}
+    wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=${fileid[$i]}' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=${fileid[$i]}" -O ${filename[$i]} && rm -rf /tmp/cookies.txt
+    echo ${filename[$i]} " downloaded"
+done
