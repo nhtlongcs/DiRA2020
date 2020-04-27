7691c75c07ed3fef2bf5043dddad7fced5de4680
Loi Ly <vinhloiit1327@gmail.com>
2020-04-27 16:59:44 +0700

    add loop param, add default params

--- camera_record/scripts/publish.py
+++ camera_record/scripts/publish.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 import os
 import cv2
 import sys
@@ -6,7 +6,6 @@ import rospy
 import rospkg
 import numpy as np
 
-from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image, CompressedImage
 
 def data_to_image(data):
@@ -27,36 +26,53 @@ class VideoPublish:
         self.rgb_frame_count = 0
         self.depth_frame_count = 0
 
-        video_path = rospy.get_param('~video_path')
+        self.video_path = rospy.get_param('~video_path', None)
+        self.loop = rospy.get_param('~loop', False)
+        image_path = rospy.get_param('~image_path', None)
         self.video_pub = rospy.Publisher('/camera/rgb/image_raw/compressed', CompressedImage, queue_size=1)
-        rospy.loginfo('Node started')
-    
+
         self.rate = rospy.Rate(15)
-        self.video = cv2.VideoCapture(video_path)
+        if self.video_path is not None:
+            self.image = None
+            self.video = cv2.VideoCapture(self.video_path)
+        elif image_path is not None:
+            self.image = cv2.imread(image_path)
+        else:
+            exit(1)
         self.pause = False
+
+        rospy.loginfo('Node started')
 
     def publish(self):
         while not rospy.is_shutdown():
             if not self.pause:
                 ret, self.frame = self.video.read()
+                # self.frame = cv2.imread('/home/ubuntu/catkin_ws/src/dira/camera_record/test.png')
                 if ret == False:
-                    rospy.logerr('Could not read frame. Stop!')
-                    exit(0)
+                    if self.loop:
+                        rospy.logwarn('End of video. Loop!!')
+                        self.video = cv2.VideoCapture(self.video_path)
+                        continue
+                    else:
+                        rospy.loginfo('Stop!')
+                        break
             
             frame = cv2.resize(self.frame, (320, 240))
-            frame = frame[:,:,0]
+            # frame = frame[:,:,0]
 
-            frame[frame > 100] = 255.0
-            frame[frame <= 100] = 0
+            # frame[frame > 100] = 255.0
+            # frame[frame <= 100] = 0
 
             msg = image_to_data(frame)
             self.video_pub.publish(msg)
 
-            cv2.imshow('temp', frame)
-            key = cv2.waitKey(1)
+            cv2.imshow('VideoRecorder', frame)
+            key = cv2.waitKey(15)
             if key == ord(' '):
                 rospy.loginfo('Pause')
                 self.pause = not self.pause
+            elif key == ord('q'):
+                break
             self.rate.sleep()
 
 if __name__ == "__main__":
