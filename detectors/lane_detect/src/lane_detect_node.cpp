f9e2d49aaa0983a74b6a086207491eef4cae631a
kento <18120134@student.hcmus.edu.vn>
2020-12-13 08:29:17 +0700

    set logger level lane detect node when debug = True

--- detectors/lane_detect/src/lane_detect_node.cpp
+++ detectors/lane_detect/src/lane_detect_node.cpp
@@ -1,4 +1,5 @@
 #include <ros/ros.h>
+#include <ros/console.h>
 #include "lane_detect/lane_detect.h"
 
 int main(int argc, char **argv)
@@ -9,6 +10,14 @@ int main(int argc, char **argv)
     if (!ros::param::get("/debug_mode", is_debug))
     {
         ROS_WARN("Cannot get param debug_mode, use default = false");
+    }
+
+    if (is_debug)
+    {
+        if (ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Debug))
+        {
+            ros::console::notifyLoggerLevelsChanged();
+        }
     }
 
     LaneDetect lane_detect{is_debug};
